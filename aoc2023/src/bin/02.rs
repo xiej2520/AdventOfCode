ad97b3b7cdffcbc1f4c2233e602286e16b7ba9c0
Jacky Xie <jackyxie2520@outlook.com>
2023-12-03 12:34:31 -0500

    Day 3

--- aoc2023/src/bin/02.rs
+++ aoc2023/src/bin/02.rs
@@ -3,8 +3,8 @@
 #[allow(unused_variables)]
 use std::cmp::{max, min};
 
-use std::io::{stdin, stdout, BufWriter, Write};
 use aoc2023::UnsafeScanner;
+use std::io::{stdin, stdout, BufWriter, Write};
 
 pub fn main() {
     let mut scan = UnsafeScanner::new(stdin().lock());
@@ -14,27 +14,40 @@ pub fn main() {
 
     let (mut res_1, mut res_2) = (0, 0);
     while let Some(line) = scan.read_line() {
-        let mut i_g = line.split(": ");
-        let id: i32 = i_g.next().unwrap()[5..].parse().unwrap();
-        let games = i_g.next().unwrap().split("; ");
+        let (id, games) = line.split_once(": ").unwrap();
+        let id: i32 = id[5..].parse().unwrap();
+        let games = games.split("; ");
         let mut possible = true;
-        let (mut r_min, mut g_min, mut b_min) = (1, 1, 1);
+        let (mut r_min, mut g_min, mut b_min) = (0, 0, 0);
         for game in games {
             let items = game.split(", ");
             for item in items {
-                let mut nc = item.split(" ");
+                let mut nc = item.split(' ');
                 let n: i32 = nc.next().unwrap().parse().unwrap();
                 let color = nc.next().unwrap();
                 possible = match color {
-                    "red" => {r_min = max(r_min, n); n <= r_max}
-                    "green" => {g_min = max(g_min, n); n <= g_max}
-                    "blue" => {b_min = max(b_min, n); n <= b_max}
-                    _ => panic!()
+                    "red" => {
+                        r_min = max(r_min, n);
+                        n <= r_max
+                    }
+                    "green" => {
+                        g_min = max(g_min, n);
+                        n <= g_max
+                    }
+                    "blue" => {
+                        b_min = max(b_min, n);
+                        n <= b_max
+                    }
+                    _ => panic!(),
                 } && possible; // no short-circuit
             }
         }
-        if possible { res_1 += id }
-        res_2 += r_min * g_min * b_min;
+        if possible {
+            res_1 += id;
+        }
+        if r_min > 0 || g_min > 0 || b_min > 0 {
+            res_2 += max(r_min, 1) * max(g_min, 1) * max(b_min, 1);
+        }
     }
     writeln!(out, "{}", res_1);
     writeln!(out, "{}", res_2);
